c401e0a2da1317c1e1359fe35f22ed26276b5e17
cmendible <266546+cmendible@users.noreply.github.com>
2023-04-04 12:39:03 +0100

    sql server tag rule

--- internal/scanners/sql/rules.go
+++ internal/scanners/sql/rules.go
@@ -52,6 +52,18 @@ func (a *SQLScanner) GetRules() map[string]scanners.AzureRule {
 			},
 			Url: "https://learn.microsoft.com/en-us/azure/cloud-adoption-framework/ready/azure-best-practices/resource-abbreviations",
 		},
+		"sql-007": {
+			Id:          "sql-007",
+			Category:    "Governance",
+			Subcategory: "Use tags to organize your resources",
+			Description: "SQL should have tags",
+			Severity:    "Low",
+			Eval: func(target interface{}, scanContext *scanners.ScanContext) (bool, string) {
+				c := target.(*armsql.Server)
+				return c.Tags == nil || len(c.Tags) == 0, ""
+			},
+			Url: "https://learn.microsoft.com/en-us/azure/azure-resource-manager/management/tag-resources?tabs=json",
+		},
 	}
 }
 
