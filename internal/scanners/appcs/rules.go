d6a92a45170bb10975a342b708e3b82f8ca1781f
cmendible <266546+cmendible@users.noreply.github.com>
2023-12-13 14:54:07 +0100

    appcs purge protection rule

--- internal/scanners/appcs/rules.go
+++ internal/scanners/appcs/rules.go
@@ -113,5 +113,18 @@ func (a *AppConfigurationScanner) GetRules() map[string]scanners.AzureRule {
 			},
 			Url: "https://learn.microsoft.com/en-us/azure/azure-app-configuration/howto-disable-access-key-authentication?tabs=portal#disable-access-key-authentication",
 		},
+		"appcs-009": {
+			Id:          "appcs-009",
+			Category:    scanners.RulesCategoryReliability,
+			Subcategory: scanners.RulesSubcategoryReliabilityReliability,
+			Description: "AppConfiguration should have purge protection enabled",
+			Severity:    scanners.SeverityMedium,
+			Eval: func(target interface{}, scanContext *scanners.ScanContext) (bool, string) {
+				c := target.(*armappconfiguration.ConfigurationStore)
+				purgeProtection := c.Properties.EnablePurgeProtection != nil && *c.Properties.EnablePurgeProtection
+				return !purgeProtection, ""
+			},
+			Url: "https://learn.microsoft.com/en-us/azure/azure-app-configuration/concept-soft-delete#purge-protection",
+		},
 	}
 }
