a7ea7fd753b5466b0dabd736d2a46f5557dd4925
cmendible <266546+cmendible@users.noreply.github.com>
2024-03-07 23:19:21 +0100

    Update dependencies

--- internal/scanners/defender.go
+++ internal/scanners/defender.go
@@ -4,9 +4,11 @@
 package scanners
 
 import (
-	"github.com/rs/zerolog/log"
+	"fmt"
 	"strconv"
 	"strings"
+
+	"github.com/rs/zerolog/log"
 
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/security/armsecurity"
 )
@@ -48,7 +50,7 @@ func (r DefenderResult) ToMap(mask bool) map[string]string {
 func (s *DefenderScanner) Init(config *ScannerConfig) error {
 	s.config = config
 	var err error
-	s.client, err = armsecurity.NewPricingsClient(config.SubscriptionID, config.Cred, config.ClientOptions)
+	s.client, err = armsecurity.NewPricingsClient(config.Cred, config.ClientOptions)
 	if err != nil {
 		return err
 	}
@@ -59,7 +61,7 @@ func (s *DefenderScanner) Init(config *ScannerConfig) error {
 func (s *DefenderScanner) ListConfiguration() ([]DefenderResult, error) {
 	LogSubscriptionScan(s.config.SubscriptionID, "Defender Status")
 	if s.defenderFunc == nil {
-		resp, err := s.client.List(s.config.Ctx, nil)
+		resp, err := s.client.List(s.config.Ctx, fmt.Sprintf("subscriptions/%s", s.config.SubscriptionID), nil)
 		if err != nil {
 			if strings.Contains(err.Error(), "ERROR CODE: Subscription Not Registered") {
 				log.Info().Msg("Subscription Not Registered for Defender. Skipping Defender Scan...")
