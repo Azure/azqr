c5b3d3a910f298ebee25f93e1a9c07289fd47763
Carlos Mendible <266546+cmendible@users.noreply.github.com>
2024-10-02 11:22:37 +0000

    fixed pbi template

--- internal/renderers/pbi/pbi.go
+++ internal/renderers/pbi/pbi.go
@@ -6,23 +6,17 @@ package pbi
 import (
 	"os"
 	"path/filepath"
-	"runtime"
 
 	"github.com/Azure/azqr/internal/embeded"
 	"github.com/rs/zerolog/log"
 )
 
 func CreatePBIReport(path string) {
-	if runtime.GOOS != "windows" {
-		log.Info().Msg("Skipping PowerBI report generation. Since it's only supported on Windows")
-		return
-	}
-
 	if path == "" {
 		log.Fatal().Msg("Please specify the path were the PowerBI template will be created using --template-path option")
 	}
 
-	log.Info().Msgf("Generating Power BI dashboard template: %s.pbit", path)
+	log.Info().Msgf("Generating Power BI dashboard template: %sazqr.pbit", path)
 
 	pbit := embeded.GetTemplates("azqr.pbit")
 	err := os.WriteFile(filepath.Join(path, "azqr.pbit"), []byte(pbit), 0644)
