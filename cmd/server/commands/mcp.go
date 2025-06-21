dbbbbd2ec68ee914bb7326072e177de23652e65c
cmendible <266546+cmendible@users.noreply.github.com>
2025-06-21 22:04:51 +0200

    new mcp server implementation

--- cmd/server/commands/mcp.go
+++ cmd/server/commands/mcp.go
@@ -4,14 +4,17 @@
 package commands
 
 import (
+	"context"
+	"encoding/base64"
 	"fmt"
+	"log"
+	"os"
 
 	"github.com/Azure/azqr/internal"
 	"github.com/Azure/azqr/internal/models"
 	"github.com/Azure/azqr/internal/renderers"
-	"github.com/invopop/jsonschema"
-	mcp_golang "github.com/metoro-io/mcp-golang"
-	"github.com/metoro-io/mcp-golang/transport/stdio"
+	"github.com/mark3labs/mcp-go/mcp"
+	"github.com/mark3labs/mcp-go/server"
 	"github.com/spf13/cobra"
 )
 
@@ -25,89 +28,177 @@ var mcpCmd = &cobra.Command{
 	Long:  "Start the MCP server",
 	Args:  cobra.NoArgs,
 	Run: func(cmd *cobra.Command, args []string) {
-		mcp(cmd)
+		run(cmd)
 	},
 }
 
-type EmptyArguments struct {
-	// No arguments needed
+var s *server.MCPServer
+
+// mcp starts the MCP server using mark3labs/mcp-go.
+// It registers the azqr tools and serves requests over stdio.
+func run(cmd *cobra.Command) {
+	s = server.NewMCPServer(
+		"Azure Quick Review 🚀",
+		"1.0.0",
+		server.WithToolCapabilities(false),
+	)
+
+	types := mcp.NewResource(
+		"docs://types",
+		"Azure Quick Review Supported Types",
+		mcp.WithResourceDescription(`List all supported recommendations. This command returns details of the Recommendations
+			supported by Azure Quick Review (azqr). Use this to explore recommendations per id, category, impact and resource type.`),
+		mcp.WithMIMEType("text/markdown"),
+	)
+
+	// Add tool handler
+	s.AddResource(types, typesHandler)
+
+	r := mcp.NewResource(
+		"docs://recommendations",
+		"Azure Quick Review Supported recommendations",
+		mcp.WithResourceDescription(`List all supported recommendations. This command returns details of the Recommendations
+			supported by Azure Quick Review (azqr). Use this to explore recommendations per id, category, impact and resource type.`),
+		mcp.WithMIMEType("text/markdown"),
+	)
+
+	s.AddResource(r, recommendationsHandler)
+
+	// Create a new resource to get the current working directory
+	currentFolder := mcp.NewResource(
+		"cli://current-folder",
+		"Current Working Directory",
+		mcp.WithResourceDescription("Returns the current working directory of the server process."),
+		mcp.WithMIMEType("text/plain"),
+	)
+
+	// Register the resource and its handler
+	s.AddResource(currentFolder, currentFolderHandler)
+
+	scan := mcp.NewTool("scan",
+		mcp.WithDescription(`Run an Azure Quick Review (azqr) scan for a given reource type.`),
+		mcp.WithString("serviceKey",
+			mcp.Required(),
+			mcp.Description("Type abbreviation of the resource type to scan, e.g. 'aks' for Azure Kubernetes Service."),
+		),
+	)
+
+	s.AddTool(scan, scanHandler)
+
+	// Start the stdio server
+	if err := server.ServeStdio(s); err != nil {
+		fmt.Printf("Server error: %v\n", err)
+	}
 }
 
-type ScanArguments struct {
-	ServiceKey string `json:"key" jsonschema:"required,description=The abrreviation of the resource type to scan"`
+func typesHandler(ctx context.Context, request mcp.ReadResourceRequest) ([]mcp.ResourceContents, error) {
+	st := renderers.SupportedTypes{}
+	output := st.GetAll()
+
+	return []mcp.ResourceContents{
+		mcp.TextResourceContents{
+			URI:      "docs://types",
+			MIMEType: "text/markdown",
+			Text:     output,
+		},
+	}, nil
 }
 
-func mcp(cmd *cobra.Command) {
-	done := make(chan struct{})
+func recommendationsHandler(ctx context.Context, request mcp.ReadResourceRequest) ([]mcp.ResourceContents, error) {
+	output := renderers.GetAllRecommendations(true)
 
-	server := mcp_golang.NewServer(stdio.NewStdioServerTransport())
-
-	jsonschema.Version = "https://json-schema.org/draft-07/schema"
-
-	// Register the new tool in the MCP server
-	// Register the "types" tool in the MCP server with a multiline description.
-	err := server.RegisterTool(
-		"types",
-		`List all supported resource types. This commnands returns details of the Azure Resource Types
-		suported by Azure Quick rteview (azqr). Use this to explore supported services and their abbrevaitions.
-		The output is a markdown table.`,
-		func(arguments EmptyArguments) (*mcp_golang.ToolResponse, error) {
-			// Create a SupportedTypes instance and get all supported types.
-			st := renderers.SupportedTypes{}
-			output := st.GetAll()
-			return mcp_golang.NewToolResponse(mcp_golang.NewTextContent(output)), nil
+	return []mcp.ResourceContents{
+		mcp.TextResourceContents{
+			URI:      "docs://recommendations",
+			MIMEType: "text/markdown",
+			Text:     output,
 		},
-	)
+	}, nil
+}
+
+func currentFolderHandler(ctx context.Context, request mcp.ReadResourceRequest) ([]mcp.ResourceContents, error) {
+	currentDir, err := getCurrentFolder()
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 
-	err = server.RegisterTool(
-		"recommendations",
-		`List all supported recommendations. This command returns details of the Recommendations
-		supported by Azure Quick Review (azqr). Use this to explore recommendations per id, category, impact and resource type.`,
-		func(arguments EmptyArguments) (*mcp_golang.ToolResponse, error) {
-			output := renderers.GetAllRecommendations(true)
-			return mcp_golang.NewToolResponse(mcp_golang.NewTextContent(output)), nil
+	return []mcp.ResourceContents{
+		mcp.TextResourceContents{
+			URI:      "cli://current-folder",
+			MIMEType: "text/plain",
+			Text:     currentDir,
 		},
+	}, nil
+}
+
+func scanHandler(ctx context.Context, request mcp.CallToolRequest) (*mcp.CallToolResult, error) {
+	serviceKey, err := request.RequireString("serviceKey")
+	if err != nil {
+		return mcp.NewToolResultError(err.Error()), nil
+	}
+
+	// output := "Scan started, please wait for the results."
+
+	// go func() {
+	currentDir, err := getCurrentFolder()
+	if err != nil {
+		log.Fatal(fmt.Errorf("failed to get current working directory: %w", err))
+	}
+
+	scannerKeys := []string{serviceKey}
+	filters := models.LoadFilters("", scannerKeys)
+	params := internal.NewScanParams()
+	params.Cost = false
+	params.Xlsx = true
+	params.Defender = false
+	params.Advisor = false
+	params.ScannerKeys = scannerKeys
+	params.Filters = filters
+	params.OutputName = currentDir + "/azqr_scan_results"
+	scanner := internal.Scanner{}
+	scanner.Scan(params)
+
+	fileName := params.OutputName + ".xlsx"
+	uri := fmt.Sprintf("file://%s", fileName)
+
+	results := mcp.NewResource(
+		uri,
+		"Azure Quick Review Scan Results",
+		mcp.WithResourceDescription(`The results of the Azure Quick Review (azqr) scan for the specified resource type.`),
+		mcp.WithMIMEType("binary/octet-stream"),
 	)
+
+	fileBlob, err := os.ReadFile(fileName)
 	if err != nil {
-		panic(err)
+		log.Fatal(fmt.Errorf("failed to read scan results file: %w", err))
 	}
 
-	err = server.RegisterTool(
-		"scan",
-		"Run an azqr scan for a given reource type.",
-		func(arguments ScanArguments) (*mcp_golang.ToolResponse, error) {
-			output := "Scan started, please wait for the results."
+	encodedBlob := make([]byte, base64.StdEncoding.EncodedLen(len(fileBlob)))
+	base64.StdEncoding.Encode(encodedBlob, fileBlob)
 
-			go func() {
-				scannerKeys := []string{arguments.ServiceKey}
-				filters := models.LoadFilters("", scannerKeys)
-				params := internal.NewScanParams()
-				params.Cost = false
-				params.Defender = false
-				params.Advisor = false
-				params.ScannerKeys = scannerKeys
-				params.Filters = filters
-				scanner := internal.Scanner{}
-				scanner.Scan(params)
-			}()
+	s.AddResource(results, func(ctx context.Context, request mcp.ReadResourceRequest) ([]mcp.ResourceContents, error) {
+		return []mcp.ResourceContents{
+			mcp.BlobResourceContents{
+				URI:      uri,
+				MIMEType: "binary/octet-stream",
+				Blob:     string(encodedBlob),
+			},
+		}, nil
+	})
 
-			return mcp_golang.NewToolResponse(mcp_golang.NewTextContent(output)), nil
-		},
-	)
+	// s.SendNotificationToClient(context.Background(),
+	// 	"notification/update",
+	// 	map[string]any{"message": fmt.Sprintf("New data available here: %s", uri)},
+	// )
+	// }()
+
+	return mcp.NewToolResultText(fileName), nil
+}
+
+func getCurrentFolder() (string, error) {
+	currentDir, err := os.Getwd()
 	if err != nil {
-		panic(err)
+		return "", fmt.Errorf("failed to get current working directory: %w", err)
 	}
-
-	err = server.Serve()
-
-	fmt.Println("Server started, waiting for requests...")
-
-	if err != nil {
-		panic(err)
-	}
-
-	<-done
+	return currentDir, nil
 }
