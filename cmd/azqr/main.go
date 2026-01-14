f52522d95e6cc1b4fd2214c5a75c5671683b22d7
cmendible <266546+cmendible@users.noreply.github.com>
2026-01-14 18:01:30 +0100

    feat: add Service Health workbook and plugin for tracking service health events

--- cmd/azqr/main.go
+++ cmd/azqr/main.go
@@ -9,6 +9,7 @@ import (
 	// Import internal plugins to register them
 	_ "github.com/Azure/azqr/internal/scanners/plugins/carbon"
 	_ "github.com/Azure/azqr/internal/scanners/plugins/openai"
+	_ "github.com/Azure/azqr/internal/scanners/plugins/servicehealth"
 	_ "github.com/Azure/azqr/internal/scanners/plugins/zone"
 )
 
