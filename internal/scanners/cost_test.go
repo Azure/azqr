31ad204f1cda0d2dcb23d0ed5ffe1c81360d83fb
cmendible <266546+cmendible@users.noreply.github.com>
2026-02-05 13:34:15 +0100

    refactor: remove previousMonth option from cost stage and related tests

--- internal/scanners/cost_test.go
+++ internal/scanners/cost_test.go
@@ -10,7 +10,7 @@ import (
 
 func TestCostTimeRangePreviousMonth(t *testing.T) {
 	now := time.Date(2026, time.February, 3, 12, 0, 0, 0, time.UTC)
-	start, end := costTimeRange(now, true)
+	start, end := costTimeRange(now)
 
 	wantStart := time.Date(2026, time.January, 1, 0, 0, 0, 0, time.UTC)
 	wantEnd := time.Date(2026, time.February, 1, 0, 0, 0, 0, time.UTC).Add(-time.Nanosecond)
@@ -21,19 +21,5 @@ func TestCostTimeRangePreviousMonth(t *testing.T) {
 
 	if !end.Equal(wantEnd) {
 		t.Fatalf("end = %v, want %v", end, wantEnd)
-	}
-}
-
-func TestCostTimeRangeDefault(t *testing.T) {
-	now := time.Date(2026, time.February, 3, 12, 0, 0, 0, time.UTC)
-	start, end := costTimeRange(now, false)
-
-	wantStart := time.Date(2025, time.November, 1, 0, 0, 0, 0, time.UTC)
-	if !start.Equal(wantStart) {
-		t.Fatalf("start = %v, want %v", start, wantStart)
-	}
-
-	if !end.Equal(now) {
-		t.Fatalf("end = %v, want %v", end, now)
 	}
 }
